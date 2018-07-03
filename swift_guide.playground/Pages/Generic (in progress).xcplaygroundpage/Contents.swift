175067401fb32449031ed1781457abe9e455f6a1
Riley Usagi <riley.usagi@gmail.com>
2018-07-03 13:06:37 +0300

    Generic init

--- swift_guide.playground/Pages/Generic (in progress).xcplaygroundpage/Contents.swift
+++ swift_guide.playground/Pages/Generic (in progress).xcplaygroundpage/Contents.swift
@@ -1,7 +1,42 @@
 //: [Назад](@previous)
-
 import Foundation
+//: ## Generic
 
-var str = "Hello, playground"
+struct Weapons<T> {
+  var items: [T] = []
+  
+  mutating func push(_ item: T) {
+    items.append(item)
+  }
+}
+
+class Knife {
+  
+}
+
+class Sword {
+  
+}
+
+class Mace {
+  
+}
+
+class Bow {
+  let name: String
+  
+  init(nme: String) {
+    self.name = nme
+  }
+}
+
+var bows = Weapons<Bow>()
+
+var shootingStar = Bow(nme: "Shooting Star")
+
+bows.push(shootingStar)
+
+bows.items
+//: ### Домашнее задание
 
 //: [Вперёд](@next)
