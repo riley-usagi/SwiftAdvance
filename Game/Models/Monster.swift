3198e27ee295879a4082650110e70ad79bd16931
Riley Usagi <riley.usagi@gmail.com>
2018-07-26 18:14:11 +0300

    Clear

--- Game/Models/Monster.swift
+++ Game/Models/Monster.swift
@@ -5,7 +5,6 @@ import RealmSwift
 let config = Realm.Configuration(deleteRealmIfMigrationNeeded: true)
 let realm = try! Realm(configuration: config)
 
-
 @objcMembers class Monster: Object, Decodable {
   dynamic var id: Int       = 0
   dynamic var name: String  = ""
@@ -59,61 +58,3 @@ let realm = try! Realm(configuration: config)
     super.init(realm: realm, schema: schema)
   }
 }
-
-//public struct MonsterOld {
-//  
-//  /// Раса монстра
-//  public enum Race: String {
-//    case plant = "Растение"
-//    case undead = "Нежить"
-//  }
-//  
-//  /// Природный элемент монстра
-//  public enum Property {
-//    case holy, poison
-//  }
-//  
-//  /// Размер монстра
-//  public enum Size {
-//    case small, medium, large
-//  }
-//  
-//  /// Имя монстра
-//  public var race: String
-//  public var property: (Property, Int)
-//  public var size: Size
-//  public var aggressive: Bool
-//  public var hit: Int
-//  public var flee: Int
-//  public var baseExp: Int
-//  public var jobExp: Int
-//  public var atk: Int
-//  public var def: Int
-//  public var mDef: Int
-//  public var str: Int
-//  public var int: Int
-//  public var agi: Int
-//  public var vit: Int
-//  public var dex: Int
-//  public var luk: Int
-//  
-//  public init() {
-//    self.race       = Race.plant.rawValue
-//    self.property   = (Property.holy, 1)
-//    self.size       = Size.small
-//    self.aggressive = false
-//    self.hit        = 37
-//    self.flee       = 90
-//    self.baseExp    = 4
-//    self.jobExp     = 5
-//    self.atk        = Int(arc4random_uniform(5) + 12)
-//    self.def        = 0
-//    self.mDef       = 0
-//    self.str        = 1
-//    self.int        = 10
-//    self.agi        = 14
-//    self.vit        = 3
-//    self.dex        = 12
-//    self.luk        = 90
-//  }
-//}
