31902a5b98387939c64d7f2428cbaf9df83442c0
Riley Usagi <riley.usagi@gmail.com>
2018-08-06 12:37:10 +0300

    Update

--- Game/Views/Characters/NewCharacter.swift
+++ Game/Views/Characters/NewCharacter.swift
@@ -13,23 +13,14 @@ class NewCharacter: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    // Do any additional setup after loading the view.
+    /// Свайп-возврат к предыдущему экрану
+    let goBackSwipe = UISwipeGestureRecognizer(target: self, action: #selector(self.respondToSwipeGesture))
+    goBackSwipe.direction = UISwipeGestureRecognizerDirection.right
+    self.view.addGestureRecognizer(goBackSwipe)
   }
   
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  /// Процесс вовзращения на предыдущий экран
+  @objc func respondToSwipeGesture(gesture: UIGestureRecognizer) {
+    dismiss(animated: true, completion: nil)
   }
-  
-  
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destinationViewController.
-   // Pass the selected object to the new view controller.
-   }
-   */
-  
 }
