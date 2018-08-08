869e0a0ceb205cba04df5f80a7112adf4115ffb2
Riley Usagi <riley.usagi@gmail.com>
2018-08-08 20:59:58 +0300

    Update

--- Game/Models/Hero.swift
+++ Game/Models/Hero.swift
@@ -8,9 +8,6 @@ let faker = Faker()
 /// Персонаж пользователя
 class Hero: Object {
   
-  /// Набор случайных имён для персонажа, если пользователь его не ввел самомстоятельно
-  // let arrayOfDefaulNames = ["Akira", "Kichi", "Mihoko", "Orino", "Shika"]
-  
   // Информация о пользователе
   @objc dynamic var id: String                = UUID().uuidString
   @objc dynamic var level: Int                = 1
@@ -19,7 +16,6 @@ class Hero: Object {
   @objc dynamic var name: String              = "" {
     didSet {
       if oldValue == "" {
-        
         // Генерируем случайное имя благодаря сторонней библиотеке
         self.name = faker.name.firstName()
       }
