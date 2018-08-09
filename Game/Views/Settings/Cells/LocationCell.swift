985e54c4c586c271c662588c0502c671f3c59f50
Riley Usagi <riley.usagi@gmail.com>
2018-08-09 10:42:47 +0300

    Update

--- Game/Views/Settings/Cells/LocationCell.swift
+++ Game/Views/Settings/Cells/LocationCell.swift
@@ -1,17 +1,16 @@
 import UIKit
+import Magic
 
 /// Ячейка с описанием текущей локации персонажа
 class LocationCell: UITableViewCell {
   
   override func awakeFromNib() {
     super.awakeFromNib()
-    // TODO: Выводить информаци о локации, на котрой находится текущий герой, в подписи к ячейке.
+//    magic(self)
   }
   
   override func setSelected(_ selected: Bool, animated: Bool) {
     super.setSelected(selected, animated: animated)
-    
-    // Configure the view for the selected state
   }
   
 }
--- Game/Views/Settings/SettingsList.swift
+++ Game/Views/Settings/SettingsList.swift
@@ -6,13 +6,13 @@ class SettingsList: UITableViewController {
   
   @IBOutlet weak var settingsTabBarItem: RAMAnimatedTabBarItem!
   
-  @IBOutlet weak var heroLocationCell: LocationCell!
+  @IBOutlet weak var currentHeroCell: LocationCell!
   
   override func viewDidLoad() {
     super.viewDidLoad()
     
     // Вывод информации о текущем герое и его локации
-    heroLocationCell.detailTextLabel?.text = characterDetail(name: Player.player.currentHero?.name, location: Player.player.currentHero?.heroLocation)
+    currentHeroCell.detailTextLabel?.text = characterDetail(name: Player.player.currentHero?.name, location: Player.player.currentHero?.heroLocation)
   }
   
   func characterDetail(name: String?, location: String?) -> String {
