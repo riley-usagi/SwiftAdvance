d8c455e93a037cdac1250a9222e9fa0cdab7dec8
Riley Usagi <riley.usagi@gmail.com>
2018-08-12 21:19:21 +0300

    Update

--- Game/Services/RealmService.swift
+++ Game/Services/RealmService.swift
@@ -1,57 +1,57 @@
-//import Foundation
-//import RealmSwift
-//
-//class RealmService {
-//  private init() {}
-//  static let shared = RealmService()
-//  
-//  var realm = try! Realm()
-//  
-//  func create<T: Object>(_ object: T) {
-//    do {
-//      try realm.write {
-//        realm.add(object)
-//      }
-//    } catch {
-//      post(error)
-//    }
-//  }
-//  
-//  func update<T: Object>(_ object: T, with dictionary: [String: Any?]) {
-//    do {
-//      try realm.write {
-//        for (key, value) in dictionary {
-//          object.setValue(value, forKey: key)
-//        }
-//      }
-//    } catch {
-//      post(error)
-//    }
-//  }
-//  
-//  func delete<T: Object>(_ object: T) {
-//    do {
-//      try realm.write {
-//        realm.delete(object)
-//      }
-//    } catch {
-//      post(error)
-//    }
-//  }
-//  
-//  func post(_ error: Error) {
-//    NotificationCenter.default.post(name: NSNotification.Name("RealmError"), object: error)
-//  }
-//  
-//  func observeRealmErrors(in vc: UIViewController, completion: @escaping (Error?) -> Void) {
-//    NotificationCenter.default.addObserver(forName: NSNotification.Name("RealmError"), object: nil, queue: nil) { (notification) in
-//      completion(notification.object as? Error)
-//    }
-//  }
-//  
-//  func stopObservingErrors(in vc: UIViewController) {
-//    NotificationCenter.default.removeObserver(vc, name: NSNotification.Name("RealmError"), object: nil)
-//  }
-//
-//}
-//
+import Foundation
+import RealmSwift
+
+class RealmService {
+  private init() {}
+  static let shared = RealmService()
+  
+  var realm = try! Realm()
+  
+  func create<T: Object>(_ object: T) {
+    do {
+      try realm.write {
+        realm.add(object)
+      }
+    } catch {
+      post(error)
+    }
+  }
+  
+  func update<T: Object>(_ object: T, with dictionary: [String: Any?]) {
+    do {
+      try realm.write {
+        for (key, value) in dictionary {
+          object.setValue(value, forKey: key)
+        }
+      }
+    } catch {
+      post(error)
+    }
+  }
+  
+  func delete<T: Object>(_ object: T) {
+    do {
+      try realm.write {
+        realm.delete(object)
+      }
+    } catch {
+      post(error)
+    }
+  }
+  
+  func post(_ error: Error) {
+    NotificationCenter.default.post(name: NSNotification.Name("RealmError"), object: error)
+  }
+  
+  func observeRealmErrors(in vc: UIViewController, completion: @escaping (Error?) -> Void) {
+    NotificationCenter.default.addObserver(forName: NSNotification.Name("RealmError"), object: nil, queue: nil) { (notification) in
+      completion(notification.object as? Error)
+    }
+  }
+  
+  func stopObservingErrors(in vc: UIViewController) {
+    NotificationCenter.default.removeObserver(vc, name: NSNotification.Name("RealmError"), object: nil)
+  }
+  
+}
+
