1f4f8f90fe9a7feed9f81e95b99f0b1bc01e284b
Riley Usagi <riley.usagi@gmail.com>
2018-08-15 12:23:34 +0300

    Update

--- Game/Views/Locations/LocationScreen.swift
+++ Game/Views/Locations/LocationScreen.swift
@@ -1,19 +1,17 @@
-//
-//  Location.swift
-//  Game
-//
-//  Created by Riley Usagi on 11.08.2018.
-//  Copyright © 2018 Riley Usagi. All rights reserved.
-//
-
 import UIKit
 
 class LocationScreen: UIViewController {
   
+  @IBOutlet weak var locationName: UILabel!
+  @IBOutlet weak var monstersList: UITableView!
+  @IBOutlet weak var mapSelect: UIButton!
+  
+  var location: Location?
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
-    // Do any additional setup after loading the view.
+    locationName.text = location?.name
   }
   
   override func didReceiveMemoryWarning() {
@@ -22,14 +20,14 @@ class LocationScreen: UIViewController {
   }
   
   
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destinationViewController.
-   // Pass the selected object to the new view controller.
-   }
-   */
+  
+  // MARK: - Navigation
+  
+  // In a storyboard-based application, you will often want to do a little preparation before navigation
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+  }
+  
   
 }
--- Game/Views/Locations/LocationsList.swift
+++ Game/Views/Locations/LocationsList.swift
@@ -5,6 +5,7 @@ import RealmSwift
 class LocationsList: UITableViewController {
   
   let locations = realm.objects(Location.self)
+  var selectedLocation: Location?
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -40,50 +41,18 @@ class LocationsList: UITableViewController {
     return cell
   }
   
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    selectedLocation = locations[indexPath.row]
+    
+  }
   
-  /*
-   // Override to support conditional editing of the table view.
-   override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-   // Return false if you do not want the specified item to be editable.
-   return true
-   }
-   */
   
-  /*
-   // Override to support editing the table view.
-   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-   if editingStyle == .delete {
-   // Delete the row from the data source
-   tableView.deleteRows(at: [indexPath], with: .fade)
-   } else if editingStyle == .insert {
-   // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-   }    
-   }
-   */
-  
-  /*
-   // Override to support rearranging the table view.
-   override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-   
-   }
-   */
-  
-  /*
-   // Override to support conditional rearranging of the table view.
-   override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-   // Return false if you do not want the item to be re-orderable.
-   return true
-   }
-   */
-  
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destinationViewController.
-   // Pass the selected object to the new view controller.
-   }
-   */
-  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    // Get the new view controller using segue.destinationViewController.
+    let locationScreenController = segue.destination as! LocationScreen
+    
+    // Pass the selected object to the new view controller.    
+    // locationScreenController.location = selectedLocation
+    locationScreenController.locationName?.text = "rwefwqef"
+  }
 }
