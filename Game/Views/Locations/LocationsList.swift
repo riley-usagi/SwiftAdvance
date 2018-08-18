bbdcee6ade8d42d0b16900a863c65e44cc0aab73
Riley Usagi <riley.usagi@gmail.com>
2018-08-18 22:12:54 +0300

    Update

--- Game/Views/Locations/LocationsList.swift
+++ Game/Views/Locations/LocationsList.swift
@@ -32,7 +32,6 @@ class LocationsList: UITableViewController {
     return locations.count
   }
   
-  
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "locationIdentifier", for: indexPath)
     
