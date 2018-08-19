0836804feb2daff918a4e4235be729e5d4612930
Riley Usagi <riley.usagi@gmail.com>
2018-08-19 20:44:14 +0300

    Update

--- Game/Views/Locations/LocationsList.swift
+++ Game/Views/Locations/LocationsList.swift
@@ -9,12 +9,6 @@ class LocationsList: UITableViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-    // Uncomment the following line to preserve selection between presentations
-    // self.clearsSelectionOnViewWillAppear = false
-    
-    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-    // self.navigationItem.rightBarButtonItem = self.editButtonItem
   }
   
   override func didReceiveMemoryWarning() {
