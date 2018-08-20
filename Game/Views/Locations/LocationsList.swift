2eac03d43fba5fc59547b2e7b866b1e6d515ac1e
Riley Usagi <riley.usagi@gmail.com>
2018-08-20 21:19:11 +0300

    Update

--- Game/Views/Locations/LocationsList.swift
+++ Game/Views/Locations/LocationsList.swift
@@ -13,16 +13,13 @@ class LocationsList: UITableViewController {
   
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
   }
   
   override func numberOfSections(in tableView: UITableView) -> Int {
-    // #warning Incomplete implementation, return the number of sections
     return 1
   }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    // #warning Incomplete implementation, return the number of rows
     return locations.count
   }
   
