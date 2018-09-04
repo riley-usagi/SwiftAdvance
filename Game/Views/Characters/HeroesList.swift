10e20ce5028e64d92bb36386b4880f0847b5a8cb
Riley Usagi <riley.usagi@gmail.com>
2018-09-04 09:02:57 +0300

    Update

--- Game/Views/Characters/HeroesList.swift
+++ Game/Views/Characters/HeroesList.swift
@@ -6,20 +6,34 @@ class HeroesList: UITableViewController {
   
   let heroes = realm.objects(Hero.self)
   
-  var gameResults: [Int]?
-  
   override func viewDidLoad() {
     super.viewDidLoad()
   }
   
+  /// Количество секций (должна быть минимум одна)
+  ///
+  /// - Parameter tableView: объект таблицы
+  /// - Returns: колицкство секций (минимум одна)
   override func numberOfSections(in tableView: UITableView) -> Int {
     return 1
   }
   
+  /// Количество ячеек
+  ///
+  /// - Parameters:
+  ///   - tableView: объект таблицы
+  ///   - section: секция
+  /// - Returns: количество ячеек
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
     return heroes.count
   }
     
+  /// Настрока каждой конкретной ячейки
+  ///
+  /// - Parameters:
+  ///   - tableView: объект таблицы
+  ///   - indexPath: номер ячейки
+  /// - Returns: объект ячейки
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "heroIdentifier", for: indexPath)
     
@@ -32,6 +46,11 @@ class HeroesList: UITableViewController {
     return cell
   }
   
+  /// Выбор ячейки
+  ///
+  /// - Parameters:
+  ///   - tableView: объект таблицы
+  ///   - indexPath: номер ячейки
   override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
     Player.shared.currentHero = heroes[indexPath.row]
   }
